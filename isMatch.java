5051ae1a57c300ec2e4d75eebd94a5fc219af900
Angela Wu <angelang@Angelas-MacBook-Pro.local>
2016-11-03 18:37:40 -0400

    11/03

--- isMatch.java
+++ isMatch.java
@@ -32,33 +32,37 @@ public class Solution {
                            or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
 */
 public class Solution {
-    public boolean isMatch(String s, String p) {
-        if (s.length() == 0 && p.length() == 0) return true;
-        if (p.length() == 0) return false;
-        int slen = s.length(), plen = p.length();
-        boolean[][] dp = new boolean[slen+1][plen+1];
-        dp[0][0] = true;
-        for (int i = 0; i <= p.length(); i++) {
-        	if (p.charAt(i) == '*' && dp[0][i-1]) dp[0][i] = true;
-        	else break;
-        }
-        for (int i = 0; i < slen; i++) {
-        	for (int j = 0; j < plen; j++) {
-        		if (p.charAt(j) == '*') {
-        			if (p.charAt(i-1) == '.' || s.charAt(i-1) == p.charAt(i-1)) {
-        				dp[i+1][j+1] = dp[i][j+1];
-        			} else {
-        				dp[i+1][j+1] = dp[i+1][j-1];
-        			}
-        		} else if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)){
-        			dp[i+1][j+1] = dp[i][j];
-        		} else {
-        			dp[i+1][j+1] = false;
-        		}
-        	}
-        }     
-        return dp[slen][plen];    
+public boolean isMatch(String s, String p) {
+
+    if (s == null || p == null) {
+        return false;
     }
+    boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+    dp[0][0] = true;
+    for (int i = 0; i < p.length(); i++) {
+        if (p.charAt(i) == '*' && dp[0][i-1]) {
+            dp[0][i+1] = true;
+        }
+    }
+    for (int i = 0 ; i < s.length(); i++) {
+        for (int j = 0; j < p.length(); j++) {
+            if (p.charAt(j) == '.') {
+                dp[i+1][j+1] = dp[i][j];
+            }
+            if (p.charAt(j) == s.charAt(i)) {
+                dp[i+1][j+1] = dp[i][j];
+            }
+            if (p.charAt(j) == '*') {
+                if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                    dp[i+1][j+1] = dp[i+1][j-1];
+                } else {
+                    dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                }
+            }
+        }
+    }
+    return dp[s.length()][p.length()];
+}
 }
 
 //dp
@@ -81,6 +85,28 @@ dp[i][j] = dp[i][j-1]
 3. 匹配多个元素，此时p[0: j-1] = { p[0: j-2], p[j-2], ... , p[j-2] }
 dp[i][j] = dp[i-1][j] && (p[j-2]=='.' || s[i-2]==p[j-2])
 */
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i > 0 && p.charAt(j-1) != '.' && p.charAt(j-1) != '*') {
+                    dp[i][j] = dp[i-1][j-1] && (s.charAt(i-1) == p.charAt(j-1));
+                } else if (p.charAt(j-1) == '.') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (j > 1) {
+                    if ((dp[i][j-2] || dp[i][j-1])) dp[i][j] = true;
+                    else if (i > 0 && dp[i-1][j] && (p.charAt(j-2) == '.' || s.charAt(i-1) == p.charAt(j-2)))  dp[i][j] = true;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
 
 
 
