299034fed17b09607d331d84f0bcd851215622a3
Angela Wu <fangting1031@hotmail.com>
2017-08-09 20:19:21 -0700

    add a new method

--- merge.java
+++ merge.java
@@ -1,3 +1,16 @@
+//O(n)
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
+        }
+        while (j >= 0) nums1[k--] = nums2[j--];
+    }
+}
+
+//O(mnlog(n))
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         if (nums1.length == 0) nums1 = nums2;
