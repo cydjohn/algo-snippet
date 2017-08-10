031f47ced67d82d36962c9dd8643e65251b514e7
Angela Wu <fangting1031@hotmail.com>
2017-08-09 21:44:53 -0700

    improve with recursive method

--- lowestCommonAncestor2.java
+++ lowestCommonAncestor2.java
@@ -9,6 +9,15 @@
  * }
  */
 public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
+
+public class Solution {
     boolean found = false;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         List<TreeNode> p1 = new ArrayList();
