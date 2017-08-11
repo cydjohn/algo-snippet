bd61adc47982df5d56f4faf7d25f82fe26d9a1b6
Angela Wu <fangting1031@hotmail.com>
2017-08-10 21:17:21 -0700

    improve the code

--- removeDuplicates2.java
+++ removeDuplicates2.java
@@ -1,3 +1,16 @@
+// Just go through the numbers and include those in the result that haven't been included twice already.
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums) {
+            if (i < 2 || n > nums[i-2]) {
+                nums[i++] = n; 
+            }
+        }
+        return i;
+    }
+}
+
 //insertion + cnt记录重复的次数
 public class Solution {
     public int removeDuplicates(int[] nums) {
