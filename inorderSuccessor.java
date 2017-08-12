328551e01d1ff588aee0a7a2891a1a68e16c81c6
Angela Wu <fangting1031@hotmail.com>
2017-08-11 20:29:10 -0700

    add log(n) method

--- inorderSuccessor.java
+++ inorderSuccessor.java
@@ -7,7 +7,32 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-//dfs
+//BST Character O(logn)
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) return null;
+        if (p.right != null) {
+            TreeNode res = p.right;
+            while (res.left != null) res = res.left;
+            return res;
+        } else {
+            TreeNode res = root;
+            TreeNode lastLeft = null;
+            while (res != null) {
+                if (res == p || res == null) break;
+                if (res.val > p.val) {
+                    lastLeft = res;
+                    res = res.left;   
+                }
+                else res = res.right;
+            }
+            if (res == p) return lastLeft;
+            else return null;
+        }
+    }
+}
+
+//dfs O(V+E)
 public class Solution {
 	boolean foundp = false;
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
